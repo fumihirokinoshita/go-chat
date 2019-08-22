920c4343bbb1cf7d80104c472e7779ab6da29e2b
mihiro <f.mihiroxx@gmail.com>
2019-08-23 07:07:46 +0900

    Add run method in room.go

--- room.go
+++ room.go
@@ -10,3 +10,29 @@ type room struct {
 	// clientsは在室している全てのクライアントが保持される
 	clients map[*client]bool
 }
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			// 参加
+			r.clients[client] = true
+		case client := <-r.leave:
+			// 退室
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			// 全てのクライアントにメッセージを転送
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					// メッセージを送信
+				default:
+					// 送信に失敗
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
+}
