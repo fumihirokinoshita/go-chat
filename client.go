36ad6bbd4d726e4f2825737813791e49f4f2bc62
mihiro <f.mihiroxx@gmail.com>
2019-08-23 06:48:05 +0900

    Add read & write method in client.go

--- client.go
+++ client.go
@@ -13,3 +13,23 @@ type client struct {
 	// room is the chatroom in which this client is participating.
 	room *room
 }
+
+func (c *client) read() {
+	for {
+		if _, msg, err := c.socket.ReadMessage(); err == nil {
+			c.room.forward <- msg
+		} else {
+			break
+		}
+	}
+	c.socket.Close()
+}
+
+func (c *client) write() {
+	for msg := range c.send {
+		if err := c.socket.WriteMessage(websocket.TextMessage, msg); err != nil {
+			break
+		}
+	}
+	c.socket.Close()
+}
