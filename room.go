7b88851828194f6bf0572c99d7d3ad1a1cd3acd9
mihiro <f.mihiroxx@gmail.com>
2019-08-23 06:59:55 +0900

    Add chat room model in room.go
    join chan
    leave chan
    clients map

--- room.go
+++ room.go
@@ -3,4 +3,10 @@ package main
 type room struct {
 	// forwardは他のクライアントに転送するためのメッセージを保持するチャネル
 	forward chan []byte
+	// joinはチャットルームに参加しようとしているクライアントのためのチャネル
+	join chan *client
+	// leaveはチャットルームから退室しようとしているクライアントのためのチャネル
+	leave chan *client
+	// clientsは在室している全てのクライアントが保持される
+	clients map[*client]bool
 }
