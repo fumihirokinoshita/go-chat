a61b494abe3fb2a229a8cd2b852ab97a085a1a32
mihiro <f.mihiroxx@gmail.com>
2019-08-23 06:16:39 +0900

    Add http.Handle

--- main.go
+++ main.go
@@ -23,18 +23,9 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-			<html>
-				<head>
-					<title>チャット</title>
-				</head>
-				<body>
-					チャットしよう！
-				</body>
-			</html>
-		`))
-	})
+	// root
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+
 	// start web server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndserve:", err)
