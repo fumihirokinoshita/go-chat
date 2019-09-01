6cf036ddc237746a40f521f65c6713a78e4ad69e
mihiro <f.mihiroxx@gmail.com>
2019-09-01 22:44:36 +0900

    Add avatar test

--- chat/avatar.go
+++ chat/avatar.go
@@ -13,3 +13,16 @@ type Avatar interface {
 	// 問題がガッ生した場合はerrorを返す。特にURLを取得できなかった場合はErrNoAvatarURLを返す
 	getAvatarURL(c *client) (string, error)
 }
+
+type AuthAvatar struct{}
+
+var UseAuthAvatar AuthAvatar
+
+func (_ AuthAvatar) GetAvatarURL(c *client) (string, error) {
+	if url, ok := c.userData["avatar_url"]; ok {
+		if urlStr, ok := url.(string); ok {
+			return urlStr, nil
+		}
+	}
+	return "", ErrNoAvatarURL
+}
